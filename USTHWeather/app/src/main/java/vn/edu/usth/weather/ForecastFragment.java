c3f7813911563d74457125f372190ff069ada1cb
QK04 <khanhnq.22bi13211@usth.edu.vn>
2024-09-04 23:23:46 +0700

    do something about linearlayout

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -13,30 +13,9 @@ import android.widget.TextView;
 
 
 public class ForecastFragment extends Fragment {
-    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(Color.parseColor("#20FF0000"));
-
-        LinearLayout layout = new LinearLayout(getContext());
-        layout.setOrientation(LinearLayout.VERTICAL);
-
-        TextView textView = new TextView(getContext());
-        textView.setText("Thursday");
-        textView.setTextSize(24);
-        textView.setTextColor(Color.BLACK);
-
-        
-        ImageView imageView = new ImageView(getContext());
-        imageView.setImageResource(R.drawable.sun);
-        imageView.setLayoutParams(new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        layout.addView(textView);
-        layout.addView(imageView);
-        return layout;
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
     }
 }
