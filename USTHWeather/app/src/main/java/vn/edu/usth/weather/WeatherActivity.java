77c86336cf2a9877bb2d1165fe6e01f49d27f89e
QK04 <khanhnq.22bi13211@usth.edu.vn>
2024-10-02 22:25:51 +0700

    create toolbar

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,9 +1,13 @@
 package vn.edu.usth.weather;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-import com.google.android.material.tabs.TabLayout;
+import androidx.appcompat.widget.Toolbar;
 
 public class WeatherActivity extends AppCompatActivity {
     @Override
@@ -11,15 +15,32 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setOffscreenPageLimit(3);
+//        ViewPager viewPager = findViewById(R.id.view_pager);
+//        viewPager.setOffscreenPageLimit(3);
+//
+//        TabLayout tabLayout = findViewById(R.id.tab_layout);
+//        tabLayout.setupWithViewPager(viewPager);
 
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
+        Toolbar toolbar = findViewById(R.id.myToolBar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
-        WeatherPagerAdapter weatherPagerAdapter = new WeatherPagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(weatherPagerAdapter);
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.refresh){
+            Toast.makeText(this, "Refresh clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.share) {
+            Intent intent = new Intent(this, PrefActivity.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
