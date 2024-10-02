ee21c8b9e05656271b0c1c639cb8c30a882f9ccd
QK04 <khanhnq.22bi13211@usth.edu.vn>
2024-10-02 22:49:13 +0700

    Use thread and handler, simulate a network request and show on a toast

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -2,28 +2,40 @@ package vn.edu.usth.weather;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
 
-//        ViewPager viewPager = findViewById(R.id.view_pager);
-//        viewPager.setOffscreenPageLimit(3);
-//
-//        TabLayout tabLayout = findViewById(R.id.tab_layout);
-//        tabLayout.setupWithViewPager(viewPager);
+        setContentView(R.layout.activity_weather);
 
         Toolbar toolbar = findViewById(R.id.myToolBar);
         setSupportActionBar(toolbar);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setOffscreenPageLimit(3);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        WeatherPagerAdapter weatherPagerAdapter = new WeatherPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(weatherPagerAdapter);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -32,8 +44,8 @@ public class WeatherActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.refresh){
-            Toast.makeText(this, "Refresh clicked", Toast.LENGTH_SHORT).show();
+        if (item.getItemId() == R.id.refresh) {
+            simulateNetworkRequest();
             return true;
         } else if (item.getItemId() == R.id.share) {
             Intent intent = new Intent(this, PrefActivity.class);
@@ -42,5 +54,32 @@ public class WeatherActivity extends AppCompatActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void simulateNetworkRequest() {
+        final Handler handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                String content = msg.getData().getString("server_response");
+                Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Bundle bundle = new Bundle();
+                bundle.putString("server_response", "Weather data refreshed");
+                Message msg = new Message();
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+            }
+        });
+        t.start();
     }
 }
